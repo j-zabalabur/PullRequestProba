0e17818d032df74bd8d8564826543e7ee8d2d654
Jon Zabala <j_zabalabur@fpzornotza.com>
2024-11-27 09:01:38 +0100

    Proba

--- Prueba/MProba.php
+++ Prueba/MProba.php
@@ -1,8 +1,10 @@
 <?php
 $zenb1=3;
 $zenb2=4;
+$zenb3=4;
 echo nl2br("Lehen zenbakia: ".$zenb1."\nBigarren zenbakia: ".$zenb2."\n");
-echo nl2br("Batuketa: ".$zenb1+$zenb2."\n");
+echo nl2br("Batuketa: ".$zenb1+$zenb2+$zenb3."\n");
+$guztira=$zenb1+$zenb2+$zenb3;
 echo nl2br("Kenketa: ".$zenb1-$zenb2."\n");
 echo nl2br("Biderketa: ".$zenb1*$zenb2."\n");
 echo nl2br("Zatiketa: ".$zenb1/$zenb2."\n");
