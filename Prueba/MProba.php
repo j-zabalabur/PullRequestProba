9d454f73e94008e425f4356c7cf90945d417139e
2AW3 User 22 <2AW3-22@yourdomain.com>
2024-11-27 09:02:43 +0100

    MProba modificado

--- Prueba/MProba.php
+++ Prueba/MProba.php
@@ -4,6 +4,27 @@ $zenb2=7;
 echo nl2br("Lehen zenbakia: ".$zenb1."\nBigarren zenbakia: ".$zenb2."\n");
 echo nl2br("Batuketa: ".$zenb1+$zenb2."\n");
 echo nl2br("Kenketa: ".$zenb1-$zenb2."\n");
+?>
+
+<?php
+    class Conexion{
+        private $con;
+
+        public function __construct() {
+            $this->con = new mysqli("localhost", "root", "", "retrobits");
+        }
+        
+        public function getCon(){
+            return $this->con;
+        }
+
+        public function closeCon(){
+            return $this->con->close();
+        }
+    }
+
+?>
+<?php
 echo nl2br("Biderketa: ".$zenb1*$zenb2."\n");
 echo nl2br("Zatiketa: ".$zenb1/$zenb2."\n");
 echo "Hondarra: ".$zenb1%$zenb2;
