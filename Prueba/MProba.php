b8c95e02d816f4176a1302b1f5f4a3f16ab8cd63
Jon Zabala <j_zabalabur@fpzornotza.com>
2024-11-27 08:46:38 +0100

    Primer commit

---
+++ Prueba/MProba.php
@@ -0,0 +1,12 @@
+<?php
+$zenb1=3;
+$zenb2=4;
+echo nl2br("Lehen zenbakia: ".$zenb1."\nBigarren zenbakia: ".$zenb2."\n");
+echo nl2br("Batuketa: ".$zenb1+$zenb2."\n");
+echo nl2br("Kenketa: ".$zenb1-$zenb2."\n");
+echo nl2br("Biderketa: ".$zenb1*$zenb2."\n");
+echo nl2br("Zatiketa: ".$zenb1/$zenb2."\n");
+echo "Hondarra: ".$zenb1%$zenb2;
+echo "<br>";
+echo "Lerro jauziaren proba";
+?>
